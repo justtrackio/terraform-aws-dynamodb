ea49ae8f0b31f11b172776a004d83f9887c4698d
Bünyamin Dökmetaş <19335284+ztzxt@users.noreply.github.com>
2023-11-15 15:28:50 +0100

    fix: Define ttl_attribute_name only when ttl is enabled

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+locals {
+  ttl_attribute_name = var.ttl_enabled ? var.ttl_attribute_name : ""
+}
+
 resource "aws_dynamodb_table" "this" {
   count = var.create_table && !var.autoscaling_enabled ? 1 : 0
 
@@ -14,7 +18,7 @@ resource "aws_dynamodb_table" "this" {
 
   ttl {
     enabled        = var.ttl_enabled
-    attribute_name = var.ttl_attribute_name
+    attribute_name = local.ttl_attribute_name
   }
 
   point_in_time_recovery {
